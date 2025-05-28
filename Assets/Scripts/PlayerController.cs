7b597b97987c8eb19e2d27224b08b92d59ae60eb
Görkem Canıdemir <40427109+gcanidemir@users.noreply.github.com>
2025-05-28 20:56:31 +0300

    src uploaded

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float speed = 20.0f;
+    private float xRange = 15.0f;
+    public GameObject projectilePrefab;
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Keep the player in bounds
+        if (transform.position.x < -xRange)
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        if (transform.position.x > xRange)
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
